55b7416cd0c0b41ba3847d9ec078f683a6487b5e
Nick Plekhanov <nickatgit@yandex.com>
2016-06-15 23:38:39 +0200

    Amend according to code style.

--- bin/clean.js
+++ bin/clean.js
@@ -2,14 +2,14 @@ var rimraf = require('rimraf');
 var path = require('path');
 var fs = require('fs');
 
-var dir = path.resolve(__dirname,'../build');
+var dir = path.resolve(__dirname, '../build');
 
 rimraf(dir, function (err) {
-	if (err) {
-		throw err;
-	}
-	
-	if (process.argv.indexOf("createdir") >= 0) {
-		fs.mkdirSync(dir);
-	}
+  if (err) {
+    throw err;
+  }
+
+  if (process.argv.indexOf('createdir') >= 0) {
+    fs.mkdirSync(dir);
+  }
 });
