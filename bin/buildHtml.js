c2f86d0eab2c7833ec5e34e97761a0446e5b9c6b
Nick Plekhanov <nickatgit@yandex.com>
2016-09-01 20:53:00 +0200

    Clean up work for bin scripts

--- bin/buildHtml.js
+++ bin/buildHtml.js
@@ -1,5 +1,5 @@
 /**
- * This script copies src/index.html into build/index.html
+ * This script copies src/client/assets/index.html into build/index.html
  * This is a good example of using Node and cheerio to do a simple file transformation.
  * It might be useful when we only want to do smth. specific in the built production code.
  */
@@ -10,21 +10,21 @@ const fs = require('fs');
 const colors = require('colors');
 const cheerio = require('cheerio');
 
-fs.readFile('src/index.html', 'utf8', (err, markup) => {
+fs.readFile('src/client/assets/index.html', 'utf8', (err, markup) => {
   if (err) {
     return console.log(err);
   }
 
   const $ = cheerio.load(markup);
 
-  // Since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
+  // Since a separate spreadsheet is only utilized for the production build, need to dynamically add this here
   $('head').append('<link rel="stylesheet" href="/css/app.css">');
 
-  fs.writeFile('build/index.html', $.html(), 'utf8', function (err) {
+  fs.writeFile('build/client/index.html', $.html(), 'utf8', (err) => {
     if (err) {
       return console.log(err);
     }
   });
 
-  console.log('index.html written to /build'.green);
+  console.log('index.html written to /build/client'.green);
 });
